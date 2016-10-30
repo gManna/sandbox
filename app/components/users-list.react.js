108094e0e7a3312ce5a76d9915148048345b84fd
gaejin <gaejin@gmail.com>
2016-10-30 18:30:28 +0100

    spostato componente user list

---
+++ app/components/users-list.react.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UserComponent from 'user.react.js';
+
+var UsersListComponent = React.createClass({
+	getInitialState: function() {
+	return {data: this.props.users};
+	},
+	render: function() {
+		var obj = this.state.data;
+		var rows = [];
+
+		return (
+			<ul>
+			{
+				obj.map(function(obj,key){
+					return <UserComponent key={key} user={obj}/>
+				})
+
+			}
+			</ul>
+		);
+	}
+});
+
+export default UsersListComponent;
